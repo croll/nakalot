e9275de6c69928f9bbccde52f01db7b21952d883
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-12-13 18:53:58 +0100

    add parameters page in routes

--- app/Routes.js
+++ app/Routes.js
@@ -4,11 +4,13 @@ import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import ParametersPage from './containers/ParametersPage';
 
 export default () => (
   <App>
     <Switch>
       <Route path={routes.COUNTER} component={CounterPage} />
+      <Route path={routes.PARAMETERS} component={ParametersPage} />
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
