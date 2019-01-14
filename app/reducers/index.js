a662c40991dc489aeb72f71a891c997cf88bd5c0
Nicolas Dimitrijevic <nicolas@croll.fr>
2019-01-14 20:51:01 +0100

    add parameters and instances in reducers dependencies

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,10 +2,14 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import counter from './counter';
+import parameters from './parameters';
+import instances from './instances';
 
 export default function createRootReducer(history: History) {
   return combineReducers({
     router: connectRouter(history),
-    counter
+    counter,
+    parameters,
+    instances,
   });
 }
