85e67042c2bae01c65f8699ca9f5d9e1c711db97
Nicolas Dimitrijevic <nicolas@croll.fr>
2019-01-14 20:53:17 +0100

    upload page now search every levels of every tabs of xls file, and resolve them using sparql server

--- app/containers/UploadingPage.js
+++ app/containers/UploadingPage.js
@@ -7,31 +7,42 @@ import { bindActionCreators } from 'redux';
 
 import * as CounterActions from '../actions/counter';
 
+import NakalaQL from '../utils/nakalaql';
+
 class UploadingPage extends Component {
   static propTypes = {
     email: PropTypes.string,
     password: PropTypes.string,
-    handle: PropTypes.string,
+    apikey: PropTypes.string,
+    filepath: PropTypes.string,
+    labexls: PropTypes.object.isRequired,
   };
 
   static defaultProps = {
     email: '',
     password: '',
-    handle: '',
-  }
+    apikey: '',
+    filepath: '',
+  };
 
   constructor(props) {
     super(props);
     this.state = {
       ...props,
     }
+    this.nakalaql = new NakalaQL('11280/47c113f5');
+    this.uploadTabs();
   }
 
   render() {
+
+
+    //nakalaql.getMyCollections();
+
     const {
       email,
       password,
-      handle,
+      apikey,
     } = this.state;
     return (
       <div className="Uploading">
@@ -43,12 +54,42 @@ class UploadingPage extends Component {
       </div>
     );
   }
+
+  uploadTabLine = async (sheet, linenum) => {
+    let collectionHandleName = sheet['A'+linenum];
+    if (typeof(collectionHandleName) === 'object' && typeof(collectionHandleName.v) == 'string') {
+      collectionHandleName = collectionHandleName.v.trim();
+      const handle = await this.nakalaql.getCollectionHandle(collectionHandleName);
+      console.log("got handle : ", collectionHandleName, "=>", handle);
+    } else {
+    }
+  }
+
+  uploadTab = async (sheet) => {
+    const labexls = this.props.labexls;
+    const { r: rowsCount } = labexls.getSheetEnds(sheet);
+    console.log("rows : ", rowsCount);
+    for (let linenum=3; linenum<rowsCount; linenum++) {
+      await this.uploadTabLine(sheet, linenum);
+    }
+  }
+
+  uploadTabs = async () => {
+    const labexls = this.props.labexls;
+
+    for (const i_sheet in labexls.wb.Sheets) {
+      const sheet = labexls.wb.Sheets[i_sheet];
+      await this.uploadTab(sheet);
+    }
+  }
+
 }
 
 const mapStateToProps = state => ({
-  email: state.email,
-  password: state.password,
-  handle: state.handle,
+  email: state.parameters.email,
+  password: state.parameters.password,
+  apikey: state.parameters.handle,
+  labexls: state.instances.labexls,
 });
 
 function mapDispatchToProps(dispatch) {
