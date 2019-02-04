76f26ab80ecb18c5252e3ced5c8579a4213040b2
Nicolas Dimitrijevic <nicolas@croll.fr>
2019-02-04 11:05:44 +0100

    update link to documentation

--- app/components/Home.js
+++ app/components/Home.js
@@ -15,7 +15,7 @@ export default class Home extends Component<Props> {
           Bienvenue sur l'application Nakalot.
           Cette application vous permet d'envoyer plusieurs fichiers d'un coup sur Nakala.
           Pour plus d'information et documentation, consultez l'aide ici&nbsp;:&nbsp;
-          <a target="_blank" rel="noopener noreferrer" href='http://labexmed.dev.croll.fr/index.php/manuel-administrateur/nakalot/'>http://labexmed.dev.croll.fr/index.php/manuel-administrateur/nakalot/</a>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.nakala.fr/data/11280/d6273202'>https://www.nakala.fr/data/11280/d6273202</a>
         </div>
         <Link className="Start" to={routes.PARAMETERS}>Commencer...</Link>
       </div>
